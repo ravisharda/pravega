a623e0f49396a9d45b3eee0ab4f57ac65a7ff15a
Ravi Sharda <ravi.sharda@emc.com>
2019-05-27 04:39:25 -0700

    Fix checkstyle issue.
    Signed-off-by: Ravi Sharda <ravi.sharda@emc.com>

--- standalone/src/main/java/io/pravega/local/InProcPravegaCluster.java
+++ standalone/src/main/java/io/pravega/local/InProcPravegaCluster.java
@@ -42,7 +42,6 @@ import java.io.IOException;
 import java.net.URI;
 import java.util.Arrays;
 import java.util.Optional;
-import java.util.Properties;
 import java.util.UUID;
 import javax.annotation.concurrent.GuardedBy;
 import lombok.Builder;
